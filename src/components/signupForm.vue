c2b99512ea472b42d3b2825b7a9f21e659fb6175
sabin <sabinshrestha8888@gmail.com>
2022-03-28 23:44:46 +0545

    keyboard events in vue

--- src/components/signupForm.vue
+++ src/components/signupForm.vue
@@ -2,10 +2,10 @@
     <form>
         <label>Email:</label>
         <!-- using directive v-model to sync form field and data property i.e. email together -->
-        <input type="email" required v-model="email">
+        <input type="email" required v-model="email" />
 
         <label>Password:</label>
-        <input type="password" required v-model="password">
+        <input type="password" required v-model="password" />
 
         <label>Role:</label>
         <select v-model="role">
@@ -13,29 +13,19 @@
             <option value="designer">Web Designer</option>
         </select>
 
+        <label>Skills:</label>
+        <input type="text" v-model="tempSkill" @keyup="addSkill">
+        <!-- key should be unique for each skill -->
+        <div v-for="skill in skills" :key="skill" class="pill">
+            {{ skill }}
+        </div>
+
         <!-- First way to work with checkboxes, by using single 
         checkbox with v-model that would be either true or 
         false dependent on whether it's checked or not -->
         <div class="terms">
-            <input type="checkbox" v-model="terms" required>
+            <input type="checkbox" v-model="terms" required />
             <label>Accept terms and conditions</label>
-        </div>
-
-        <!-- Second way to work with checkboxes, by using an array of 
-        values & multiple different checkboxes that belong together -->
-        <div>
-            <!-- value is assigned so that its value 
-            ends up in array not boolean values -->
-            <input type="checkbox" value="shaun" v-model="names">
-            <label>Shaun</label>
-        </div>
-        <div>
-            <input type="checkbox" value="yoshi" v-model="names">
-            <label>Yoshi</label>
-        </div>
-        <div>
-            <input type="checkbox" value="mario" v-model="names">
-            <label>Mario</label>
         </div>
     </form>
 
@@ -43,7 +33,6 @@
     <p>Password: {{ password }}</p>
     <p>Role: {{ role }}</p>
     <p>Terms accepted: {{ terms }}</p>
-    <p>Names: {{ names }}</p>
 </template>
 
 <script>
@@ -52,13 +41,24 @@ export default {
         return {
             /* when a user types in input, email property 
             gets updated with value that they type in */
-            email: 'mario',
-            password: '',
-            role: '',
+            email: "mario",
+            password: "",
+            role: "",
             terms: false,
-            names: []
+            tempSkill: "",
+            skills: []
         }
-    }
+    },
+    methods: {
+        addSkill(e) {
+            // only gonna fire if tempskill has a value & we press comma
+            if (e.key === "," && this.tempSkill) {
+                this.skills.push(this.tempSkill);
+                // clear the input field
+                this.tempSkill = "";
+            }
+        },
+    },
 };
 </script>
 
@@ -82,7 +82,8 @@ label {
     font-weight: bold;
 }
 
-input, select {
+input,
+select {
     display: block;
     padding: 10px 6px;
     width: 100%;
